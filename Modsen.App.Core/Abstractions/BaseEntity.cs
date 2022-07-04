8063dad3d7b655e6dc02d877a80afa8b499f2ce1
Varvara Kostuykovich <5339504@gmail.com>
2022-07-05 02:51:39 +0300

    Changed Int type to Guid type. Changed DateTime type to DateTimeOffset.

--- Modsen.App.Core/Abstractions/BaseEntity.cs
+++ Modsen.App.Core/Abstractions/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace Modsen.App.Core.Abstractions
 {
     public abstract class BaseEntity
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         //something else???
     }
 }
