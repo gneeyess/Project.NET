c949c72b82c8df96d274fb06248cd57a0b418bb0
Varvara Kostuykovich <5339504@gmail.com>
2022-07-09 00:34:39 +0300

    Autoincrement ID at BaseEntity class

--- Dal.Entities/BaseEntity.cs
+++ Dal.Entities/BaseEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace Dal.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dal.Entities;
 
 public abstract class BaseEntity
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Required]
     public int Id { get; set; }
 }
