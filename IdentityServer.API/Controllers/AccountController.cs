0411860bd1387208120f8db96e3c1899d75611f0
Varvara Kostuykovich <5339504@gmail.com>
2022-07-08 17:56:42 +0300

    First step of registration logic. Add AccountController and RegisterModel

---
+++ IdentityServer.API/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using Dal.Entities.Identity;
+using IdentityServer.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.API.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // GET: /Account/Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    UserName = model.Email,
+                    FirstName = model.User.FirstName,
+                    LastName = model.User.LastName,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber
+                };
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return Ok();
+                    //// установка куки
+                    //await _signInManager.SignInAsync(user, false);
+                    //return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return Ok();
+        }
+    }
+}
